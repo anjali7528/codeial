8dc353a4eec9b9e20c7f99f1077cde05005ce9b7
anjali7528 <anjaligandhi248@gmail.com>
2022-03-06 16:54:48 +0530

    new commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -16,7 +16,13 @@ module.exports.home = function(req,res){
 
     Post.find({})
     .populate('user')
-    
+    .populate({
+        path: 'comments',
+        populate:{
+            path: 'user'
+        }
+
+    })
     .exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
