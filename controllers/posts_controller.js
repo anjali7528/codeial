e4b384790ebd33fd3ee0df4f8ad820e682c17b0b
anjali7528 <anjaligandhi248@gmail.com>
2022-03-07 23:53:06 +0530

    new commit

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const Comment = require('../models/comment');
 module.exports.create = function(req,res){
 Post.create({
     content: req.body.content,
@@ -13,4 +13,20 @@ Post.create({
     return res.redirect('back');
     });
 
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.paramas.id, function(err, post){
+       // .id means toconverting the obj is into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.paramas.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
